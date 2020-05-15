3529640ffdbb3d2b26fdaff3067fdf830fdc5c77
yuwei <1794287288@qq.com>
2020-05-15 18:12:40 +0800

    第二次提交

--- worldcount.c
+++ worldcount.c
@@ -24,9 +24,7 @@ int main(int argc, char *argv[])
     if (strcmp(argv[1], "-c") == 0) //统计字符数
     {
         while ((c = fgetc(fp)) != EOF)
-            // 换行符不统计
-            if (c != '\n' && c != '\r')
-                char_count++;
+            char_count++;
         fclose(fp);
         printf("字符数：%d\n", char_count);
         return 0;
@@ -39,24 +37,32 @@ int main(int argc, char *argv[])
         int len = 0;
         while (fgets(buf, 1024, fp) != NULL)
         {
-            int flag = 0;      //上一个字符是否为空格，1--是，0--否
+            if (buf[0] == '\n')
+                continue;
+            int flag = 1;      //上一个字符是否为空格或者逗号，1--是，0--否
             len = strlen(buf); //实际长度
             for (int i = 0; i < len; i++)
             {
                 c = buf[i];
-                if (c == ' ')
+                if (c == ' ' || c == ',')
                 {
                     // 上一个不是空格，但是当前是空格
+                    
                     if (flag == 0)
                     {
                         world_count++;
-                        flag = 1;
                     }
+                    flag = 1;
+                    //printf("%d\n",flag);
                 }
-                else
+                else if (c != '\n' && c != '\r' )
                 {
                     flag = 0;
                 }
+
+
+                if (i == len - 1 && flag == 0)
+                    world_count++;
             }
         }
         printf("单词数为%d\n", world_count);
